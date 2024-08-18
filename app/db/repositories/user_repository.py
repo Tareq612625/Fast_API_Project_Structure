6517853d28697c6c0a629c59f70553b763f9ef06
Tareq Aziz <t.aziz@siderian.com>
2024-08-19 03:18:12 +0600

    update

--- app/db/repositories/user_repository.py
+++ app/db/repositories/user_repository.py
@@ -3,7 +3,6 @@ from typing import List, Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from app.db.models.user import User
-from app.db.schemas.user import UserCreate
 
 class UserRepository:
     def __init__(self, session: AsyncSession):
